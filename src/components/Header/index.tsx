340df7550c6ee63c5e14e383811ad5949154a342
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-05-22 18:43:11 -0300

    Adição de header no projeto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React from "react";
+import "./styles.css";
 
 export function Header() {
-    return (
-        <p>Olá Header</p>
-    )
+  return (
+    
+
+    <section className="container-header" style={{ position: "fixed" }}>
+      <div className="mobile-content"></div>
+
+      <nav className="nav-header">
+        <ul>
+          <li>
+            <a>
+              <span>Home</span>
+            </a>
+          </li>
+
+          <li>
+            <a>
+              <span>Sobre</span>
+            </a>
+          </li>
+
+          <li>
+            <a>
+              <span>Experiência</span>
+            </a>
+          </li>
+
+          <li>
+            <a>
+              <span>Projetos</span>
+            </a>
+          </li>
+
+          <li>
+            <a>
+              <span>Resume</span>
+            </a>
+          </li>
+
+          <li>
+            <a>
+              <span>Contato</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
 }
